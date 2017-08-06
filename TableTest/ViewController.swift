7d4b314cad61618b92779ba855e550671bde05f7
DJNikka <npiterman@berkeley.edu>
2017-08-05 20:09:27 -0700

    Initial Commit

---
+++ TableTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableTest
+//
+//  Created by Konstantine Nikka-Sher Piterman on 8/5/17.
+//  Copyright © 2017 Konstantine Nikka-Sher Piterman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
